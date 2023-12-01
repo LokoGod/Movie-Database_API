56031889d5a792f46b8858b558b59a16f29a290e
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-01 14:41:40 +0530

    model

--- models/genre.ts
+++ models/genre.ts
@@ -1,18 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
 
-const main = async () => {
-  // ... you will write your Prisma Client queries here
-  const allGenres = await prisma.genre.findMany()
+const allGenres = async () => {
+  const genres = await prisma.genre.findMany();
+  return genres;
 };
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+module.exports = {
+  allGenres,
+};
