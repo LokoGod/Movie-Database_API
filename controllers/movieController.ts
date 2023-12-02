4d365e774b5ba2f8033a34d77961dfc1376afce4
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-02 19:01:30 +0530

    controller

--- controllers/movieController.ts
+++ controllers/movieController.ts
@@ -10,4 +10,23 @@ const getAllMovies = async (req: any, res: any) => {
   }
 };
 
-export { getAllMovies };
+const createMovie = async (req: any, res: any) => {
+  try {
+    const { title, genre } = req.body;
+    const movies = await movieRepository.postMovie({ title }, { genre });
+    res.status(201).json({ movies });
+  } catch (error) {}
+};
+
+const getSpecificMovie = async (req: any, res: any) => {
+  try {
+    const id = Number(req.params.id);
+    const movies = await movieRepository.specificMovie(id);
+    res.status(200).json({ movies });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export { getAllMovies, createMovie, getSpecificMovie };
