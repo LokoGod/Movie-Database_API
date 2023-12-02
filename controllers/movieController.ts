cc9d558798e06d9077a5433f24eeb1f0f92bcab7
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-02 18:54:42 +0530

    controller

--- controllers/movieController.ts
+++ controllers/movieController.ts
@@ -1,5 +1,13 @@
 import movieRepository from "../repositories/movieRepository";
 
-const getAllMovies = async (req:  any, res: any) => {
-    
-}
+const getAllMovies = async (req: any, res: any) => {
+  try {
+    const movies = await movieRepository.allMovies();
+    res.status(200).json({ movies });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export { getAllMovies };
