7c745f201728b47cf8d7a0da3b29196ba26e10a4
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-05 23:50:05 +0530

    new

--- server/controllers/directorController.ts
+++ server/controllers/directorController.ts
@@ -14,8 +14,11 @@ const getSpecificDirector = async (req: any, res: any) => {
   try {
     const id = Number(req.params.id);
     const directors = await directorRepository.specificDirector(id);
-    res.status(200).json({ directors })
-  } catch (error) {}
+    res.status(200).json({ directors });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 export { getAllDirectors };
