198e3f1b50fec64ecce9e4592adea73373e7b1d4
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-06 21:06:36 +0530

    user

--- server/repositories/userRepository.ts
+++ server/repositories/userRepository.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
+const signup = async (email: string, password: string) => {
+  const exists = await prisma.user.findUnique({ where: { email: email } });
+  if (exists) {
+    throw Error(`Email: ${exists} already in use`);
+  }
+};
 
-
-const userRepository = {  };
+const userRepository = { signup };
 export default userRepository;
