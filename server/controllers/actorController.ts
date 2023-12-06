4b1f6b7f2c628932e7336421461da3d9b9748945
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-06 12:02:46 +0530

    controller

--- server/controllers/actorController.ts
+++ server/controllers/actorController.ts
@@ -13,6 +13,23 @@ const getAllActors = async (req: any, res: any) => {
 const createActor = async (req: any, res: any) => {
   try {
     const { actor_name, movies } = req.body;
-    const actors = await actorRepository.postActor( actor_name, movies );
-  } catch (error) {}
+    const actors = await actorRepository.postActor(actor_name, movies);
+    res.status(201).json({ actors });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
+
+const getSpecificActor = async (req: any, res: any) => {
+  try {
+    const id = Number(req.params.id);
+    const actors = await actorRepository.specificActor(id);
+    res.status(200).json({ actors });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export { getAllActors, createActor, getSpecificActor };
