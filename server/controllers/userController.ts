f3c3e5a2c4f4eacca3e8822ab8caaa632d6201fe
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-06 21:27:45 +0530

    hashing

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -5,7 +5,14 @@ const loginUser = async (req: any, res: any) => {
 };
 
 const signupUser = async (req: any, res: any) => {
-  res.json({ msg: "signup-user" });
+  const { email, password } = req.body;
+  try {
+    const user = await userRepository.signup(email, password);
+    res.status(201).json({ user });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Sign-up error" });
+  }
 };
 
 export { loginUser, signupUser };
--- server/repositories/userRepository.ts
+++ server/repositories/userRepository.ts
@@ -1,11 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+import bcrypt from "bcrypt";
 
 const signup = async (email: string, password: string) => {
+    
   const exists = await prisma.user.findUnique({ where: { email: email } });
+
   if (exists) {
     throw Error(`Email: ${exists} already in use`);
   }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await prisma.user.create({ data: { email, password: hash } });
+
+  return user;
 };
 
 const userRepository = { signup };
