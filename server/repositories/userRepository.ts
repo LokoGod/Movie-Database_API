1363d46a78cbaef5e20f9e5dd83cb357ffd13b0f
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-12 20:01:41 +0530

    validation_final

--- server/repositories/userRepository.ts
+++ server/repositories/userRepository.ts
@@ -9,9 +9,11 @@ const signup = async (email: string, password: string) => {
   if (!email || !password) {
     throw Error('All fields must be filled')
   }
-
   if  (!validator.isEmail(email))  {
     throw Error('Email is not valid')
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough")
   }
     
   const exists = await prisma.user.findUnique({ where: { email: email } });
