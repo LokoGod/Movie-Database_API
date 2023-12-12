3bf96e5760f5add4c80e02799e060ebfde4fd2cc
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-12 23:06:14 +0530

    latest

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,4 +1,10 @@
 import userRepository from "../repositories/userRepository";
+import jwt from "jsonwebtoken";
+
+//Secret PW should be in the .env file 
+const createToken = (id: any) => {
+  return jwt.sign({ id }, "magesecretpasswordekathmawhottome", { expiresIn: "3d" });
+};
 
 const loginUser = async (req: any, res: any) => {
   res.json({ msg: "login-user" });
@@ -8,7 +14,9 @@ const signupUser = async (req: any, res: any) => {
   const { email, password } = req.body;
   try {
     const user = await userRepository.signup(email, password);
-    res.status(201).json({ user });
+    // create token
+    const token = createToken(user.id)
+    res.status(201).json({ user, token });
   } catch (error) {
     console.log(error);
     res.status(400).json({ error: "Sign-up error" });
